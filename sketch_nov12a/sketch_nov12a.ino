f014fff3cc0755cc001857c35a58569251d83175
Eddie <Eddie Hines>
2022-11-12 23:59:51 -0500

    Penalizing might work? I'm not sue if my code is shot or it's the hardware but the hardware was a hissy with my laptop.

--- sketch_nov12a/sketch_nov12a.ino
+++ sketch_nov12a/sketch_nov12a.ino
@@ -5,7 +5,7 @@
 // constants won't change. They're used here to set pin numbers:
 const int player1 = 2;  // pin of player 2
 const int player2 = 3;  // pin of player 1
-const int ledButtonPin = 4;  // pin of the led button
+const int LEDButtonPin = 7;  // pin of the led button
 
 
 const int LEDPin = 12;    // pin of the LED
@@ -13,13 +13,10 @@ const int LEDPin = 12;    // pin of the LED
 int buttonStateLED = 0;  // variable to store whether or not the LED button is pressed
 
 Servo wrestling_arm;  // create servo object to control a servo
-int potpin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
-int pos = 0;
 int angle = 90; // inital angle of the Servo (in the middle)
 int player1_increment = 2; // The init value of increment for pressing the button (player1)
 int player2_decrement = -2; // The init value of decrement for pressing the button (player2)
-int do_penalize = 1;
+int dont_penalize = 1;
 
 /* Check the current angle of the Servo and update it according to what player pressed the button.
   PARAMETERS
@@ -32,13 +29,12 @@ int do_penalize = 1;
     an integer indicating the new angle of the Servo, should be set to the global variable angle
 */
 int check_game_button_state(int is_positive, int angle) {
-  if (angle == 0 && !is_positive) {
-    return angle;
+  if (angle <= 0 && !is_positive) {
+    return 0;
   }
-  if (angle == 180 && is_positive) {
-    return angle;
+  if (angle >= 180 && is_positive) {
+    return 180;
   }
-  Serial.print("In button high!");
   is_positive ? angle += player1_increment : angle += player2_decrement;
   return angle;
 }
@@ -49,7 +45,11 @@ int check_game_button_state(int is_positive, int angle) {
       player 1 or player 2
 */
 void penalize(int player) {
-  player == 1 ? player2_decrement -= -1 : player1_increment += 1;
+  //return;
+  player == 1 ? player2_decrement -= 1 : player1_increment += 1;
+  Serial.println("Updating: ");
+  Serial.println(player2_decrement);
+  Serial.println(player1_increment);
 }
 
 /* These are the button handlers. When a player 1/2 button is pressed do the following:
@@ -57,15 +57,21 @@ void penalize(int player) {
   (2) Reward the player for pressing the button by changing the Servo angle
 */
 void button_handler_p1() {
-  Serial.println("Handling button press p1");
-  if(do_penalize) {penalize(1);}
+  //Serial.println("Handling button press p1");
+  Serial.println(dont_penalize);
+  //if(dont_penalize) {penalize(1);}
   angle = check_game_button_state(1, angle);
+  Serial.print(angle);
+  Serial.print("\n");
 }
 
 void button_handler_p2() {
-  Serial.println("Handling button press p2");
-  if(do_penalize) {penalize(2);}
+  //Serial.println("Handling button press p2");
+  Serial.println(dont_penalize);
+  //if(dont_penalize) {penalize(2);}
   angle = check_game_button_state(0, angle);
+  Serial.print(angle);
+  Serial.print("\n");
 }
 
 
@@ -75,7 +81,7 @@ void setup() {
   // initialize the players buttons an inputs:
   pinMode(player1, INPUT_PULLUP);
   pinMode(player2, INPUT_PULLUP);
-  pinMode(ledButtonPin, INPUT);
+  pinMode(LEDButtonPin, INPUT_PULLUP);
   // attach the interupts and trigger them when the button is released
   attachInterrupt(digitalPinToInterrupt(player1), button_handler_p1, FALLING);
   attachInterrupt(digitalPinToInterrupt(player2), button_handler_p2, FALLING);
@@ -87,17 +93,17 @@ void setup() {
 }
 
 void loop() {
-  buttonStateLED = digitalRead(ledButtonPin); // Check the LED button to see if it is pressed down
+  buttonStateLED = digitalRead(LEDButtonPin); // Check the LED button to see if it is pressed down
   // Turn the LED on/off and set the penalize flag to 0/1 (Penalize when LED isn't on)
   if(buttonStateLED == HIGH) {
-    do_penalize = 0;
+    dont_penalize = 1;
     digitalWrite(LEDPin, 0);
   } else {
-    do_penalize = 1;
+    dont_penalize = 0;
     digitalWrite(LEDPin, 1);
   }
   // Write the new angle variable to the Servo
-  Serial.print(angle);
-  Serial.print("\n");
+  //Serial.println(do_penalize);
   wrestling_arm.write(angle);
+  delay(10);
 }
